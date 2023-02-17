b73c64debbdc0014d5b09bc17f4457ea9d6128b0
Marci Garcia <marci.garcia@opensesame.com>
2023-02-17 17:12:30 -0600

    Primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,44 @@
+import "./MyLabel.css";
+
+export interface MyLabelProps {
+  /**
+   * Este es el mensaje a mostrar en la etiqueta
+   */
+  label: string;
+  /**
+   * Este es el tamaño  de la etiqueta
+   */
+  size: "normal" | "h1" | "h2" | "h3";
+
+  /**
+   * Si quiere todo capitalizado
+   */
+  allCaps?: boolean;
+
+  /**
+   * Colores basicos del label
+   */
+  color?: "primary" | "secondary" | "terciary";
+
+  /**
+   * Color Personalizado de la fuente
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  allCaps = false,
+  color = "primary",
+  label = "No Label",
+  size = "normal",
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: fontColor }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, StoryFn, StoryObj } from "@storybook/react";
+import { MyLabel } from "../../components/MyLabel";
+
+const meta = {
+  title: "UI/MyLabel",
+  component: MyLabel,
+  tags: ["autodocs"],
+  argTypes: {
+    color: {
+      control: "select",
+    },
+    fontColor: { control: "color" },
+  },
+} satisfies Meta<typeof MyLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const Template: StoryFn<typeof MyLabel> = (args) => <MyLabel {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+  size: "normal",
+  allCaps: false, // true: capitalizar toda la palabra
+};
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+  size: "normal",
+  allCaps: true, // true: capitalizar toda la palabra
+};
+export const Secondary = Template.bind({});
+Secondary.args = {
+  size: "normal",
+  color: "secondary", // true: capitalizar toda la palabra
+};
+
+export const Terciary = Template.bind({});
+Terciary.args = {
+  size: "normal",
+  color: "terciary", // true: capitalizar toda la palabra
+};
+
+export const CustomFontColor = Template.bind({});
+CustomFontColor.args = {
+  size: "h1",
+  fontColor: "#5517ac",
+};
